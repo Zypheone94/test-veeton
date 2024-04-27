08f4510193c2708ae94213bdd541ee49d21b0ba5
Stracker-Maxime <maxime@stracker.tech>
2024-04-27 21:01:22 +0200

    EVO - (CSS) : Add styled component

--- front/pages/room/[id].js
+++ front/pages/room/[id].js
@@ -1,11 +1,39 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { useRouter } from 'next/router'
 
-function room() {
+import styled from 'styled-components'
+
+function Room({className}) {
     const router = useRouter()
+    const room_id = router.query.id
+
+
+
   return (
-    <div><p>Room: {router.query.id}</p></div>
+    <div className={className}>
+        <h3>Room: {room_id}</h3>
+        <div className='message-area'>
+            message
+        </div>
+        <div>
+            <form>
+                <input placeholder='Entrer le message à envoyer'></input>
+                <button>Envoyer le message</button>
+            </form>
+        </div>
+    </div>
   )
 }
 
-export default room
+const StyledRoom = styled(Room)`
+    .message-area {
+        border: 1px solid white;
+        border-radius: 15px;
+        min-height: 250px;
+
+        margin: 50px 0;
+    }
+`
+
+
+export default StyledRoom
